0eecb82e7974a032941574803be5fbbc3b6f38d9
Tomoaki Nagahara <1668339+TomoakiNagahara@users.noreply.github.com>
2017-03-16 00:57:47 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-orm:/index.php
+ *
+ * @creation  2017-03-16
+ * @version   1.0
+ * @package   unit-orm
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+include('ORM.class.php');
+return true;
